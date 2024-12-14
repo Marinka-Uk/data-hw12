759376a3a67dc100f7b467f0c07583954b44b1fe
Marinka-Uk <129098497+Marinka-Uk@users.noreply.github.com>
2024-12-14 15:02:37 +0200

    б

--- pr/index.js
+++ pr/index.js
@@ -2,13 +2,21 @@ const list = document.querySelector('ul')
 
 
 
+const numbers = [1, 2, 3, 4, 5]
+const markup = numbers.map((number) => {
+return`<li> 
+<p>${number}</p>
+</li>`
+})
 
 
 
+[`<li><p>${number}</p></li>`, `<li>
+    <p>${number}</p></li>`,
+`<li><p>${number}</p></li>`]
 
-
-
-
+console.log(markup);
+list.insertAdjacentHTML('beforeend', markup)
 
 
 function fetchArticles(){
